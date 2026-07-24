ef8fa34723fa7eaa193c26d6a49af1c30399c3e2
Diego Henrique de Souza Ananias - Unicred do Brasil <diego.ananias@e-unicred.com.br>
2026-07-24 14:13:53 -0300

    feat(schema-manager): inject optional LoggerInterface for preload warning logs

--- src/SchemaManager.php
+++ src/SchemaManager.php
@@ -8,6 +8,7 @@ use Ananiaslitz\HyperfAvro\Exception\AvroSerializationException;
 use Apache\Avro\Schema\AvroSchema;
 use Apache\Avro\Schema\AvroSchemaParseException;
 use Hyperf\Contract\ConfigInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Finder\Finder;
 
 class SchemaManager
@@ -15,8 +16,10 @@ class SchemaManager
     protected array $schemas = [];
     protected string $schemaPath;
 
-    public function __construct(ConfigInterface $config)
-    {
+    public function __construct(
+        ConfigInterface $config,
+        protected ?LoggerInterface $logger = null,
+    ) {
         $basePath = defined('BASE_PATH') ? BASE_PATH : dirname(__DIR__);
         $this->schemaPath = $config->get('avro.schema_path', $basePath . '/storage/avro');
     }
@@ -63,6 +66,7 @@ class SchemaManager
             try {
                 $this->getSchema($name);
             } catch (AvroSerializationException $e) {
+                $this->logger?->warning("Avro SchemaManager preload failed for schema '{$name}': " . $e->getMessage());
             }
         }
     }
